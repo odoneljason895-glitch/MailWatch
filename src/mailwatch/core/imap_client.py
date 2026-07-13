cc4efeb4db9572653157f8c2dd3b669e763a3735
KZ <odoneljason895@gmail.com>
2026-07-13 17:42:03 +0200

    Add real IMAP connection support

--- src/mailwatch/core/imap_client.py
+++ src/mailwatch/core/imap_client.py
@@ -1,20 +1,35 @@
 from datetime import UTC, datetime
 
+from imapclient import IMAPClient as Client
+
+from mailwatch.accounts.models import MailAccount
 from mailwatch.core.logger import get_logger
 from mailwatch.core.models import MailMessage
-from mailwatch.accounts.models import MailAccount
 
 
 class IMAPClient:
     def __init__(self, account: MailAccount):
         self.account = account
         self.connected = False
+        self.client = None
         self.logger = get_logger("IMAPClient")
 
     def connect(self):
+        self.client = Client(
+            self.account.server,
+            port=self.account.port,
+            ssl=True,
+        )
+
+        self.client.login(
+            self.account.username,
+            self.account.password,
+        )
+
         self.connected = True
+
         self.logger.info(
-            f"Connected to {self.account.server}:{self.account.port}"
+            f"Connected to {self.account.email}"
         )
 
     def fetch_messages(self):
@@ -33,5 +48,11 @@ class IMAPClient:
         ]
 
     def disconnect(self):
-        self.connected = False
-        self.logger.info("Disconnected")
+        if self.connected and self.client:
+            self.client.logout()
+
+            self.connected = False
+
+            self.logger.info(
+                "Disconnected"
+            )
