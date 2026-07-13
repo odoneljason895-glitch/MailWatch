6f5e27576c1824846c2f534b9fa9da31d6f9488f
KZ <odoneljason895@gmail.com>
2026-07-13 17:36:47 +0200

    Connect CLI with scheduler

--- src/mailwatch/cli.py
+++ src/mailwatch/cli.py
@@ -1,16 +1,21 @@
-"""
-MailWatch command line interface.
-"""
-
 from mailwatch.core.app import MailWatchApp
 
 
 def main():
-    app = MailWatchApp("src/mailwatch/config/accounts.yml")
+    app = MailWatchApp(
+        "src/mailwatch/config/accounts.yml",
+        "src/mailwatch/config/rules.yml",
+    )
+
     app.start()
 
     print("MailWatch running")
     print(app.status())
+
+    results = app.scheduler.run_once()
+
+    print("Scheduler result:")
+    print(results)
 
 
 if __name__ == "__main__":
